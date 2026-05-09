1791f60168988e24db0b083ac97b7eb00955cea5
Aqdas-Abbasi-06 <2502114@students.au.edu.pk>
2026-05-09 12:32:03 +0500

    Added DAL, Database connection, Books, Students and BookIssues controllers

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using LMS.API.Models;
+using LMS.API.DAL;
+
+namespace LMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        
+
+
+
+
+    }
+}
---
+++ DAL/Database.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.Sqlite;
+namespace LMS.API.DAL
+{   
+    public  static class Database
+    {
+        public static string ConnectionString = "Data Source=Data/library.db";
+        public static SqliteConnection GetConnection()
+        {
+            var conn = new SqliteConnection(ConnectionString);
+            conn.Open();
+            return conn;
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
